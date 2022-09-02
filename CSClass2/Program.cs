c9a75a3eef47168a13dd210bb9f7bb52df79eb50
yujinkim0819 <a76363282@gmail.com>
2022-09-02 14:25:59 +0900

    #3 out 키워드 실습

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -18,6 +18,29 @@ namespace CSClass2
             Products p = new Products();
             Console.WriteLine(p[4]);
             p[4] = 5;
+
+            ///24-3. out 키워드
+            Console.WriteLine("숫자 입력 : ");
+            int output;
+            bool result = int.TryParse(Console.ReadLine(), out output);
+            if (result)
+            {
+                Console.WriteLine("입력한 숫자 : " + output);
+            }
+            else
+            {
+                Console.WriteLine("숫자를 입력해주세요!");
+            }
+            int x = 0; int y = 0;
+            int vx = 1; int vy = 1;
+            Console.WriteLine("현재 좌표 : (" + x + ", " + y + ")");
+            NextPos(x, y, vx, vy, out x, out y);
+            Console.WriteLine("다음 좌표 : (" + x + ", " + y + ")");
+        }
+        static void NextPos(int x, int y, int vx, int vy, out int rx, out int ry)
+        {
+            rx = x + vx;
+            ry = y + vy;
         }
     }
 }
