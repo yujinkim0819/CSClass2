23f07bffe67cb7df6e2053944f6f8a1d2b257b71
yujinkim0819 <a76363282@gmail.com>
2022-09-02 12:50:59 +0900

    초기 커밋.

---
+++ CSClass2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
