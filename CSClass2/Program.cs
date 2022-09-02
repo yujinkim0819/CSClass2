4180bd471a71163aff6d26b7043e9dfc1c8202f7
yujinkim0819 <a76363282@gmail.com>
2022-09-02 13:23:42 +0900

    Generic 실습 #1

--- CSClass2/Program.cs
+++ CSClass2/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace CSClass2
@@ -7,6 +8,12 @@ namespace CSClass2
     {
         static void Main(string[] args)
         {
+            Wanted<string> wantedString = new Wanted<string>("String");
+            Wanted<int> wantedInt = new Wanted<int>(52273);
+            Wanted<double> wantedDouble = new Wanted<double>(52.273);
+            Console.WriteLine(wantedString.Value);
+            Console.WriteLine(wantedInt.Value);
+            Console.WriteLine(wantedDouble.Value);
         }
     }
 }
---
+++ CSClass2/Wanted.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
+{
+    class Wanted<T>
+    {
+        public T Value;
+        public Wanted(T Value)
+        {
+            this.Value = Value;
+        }
+    }
+}
