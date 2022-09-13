95634dacdf6eefc4c4d73ccefdd0d2919deaa3d0
yujinkim0819 <a76363282@gmail.com>
2022-09-13 09:30:05 +0900

    #9 인터페이스 다중 상속 실습

--- CSClass2/Parent.cs
+++ CSClass2/Parent.cs
@@ -1,4 +1,10 @@
-﻿namespace CSClass2
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSClass2
 {
     class Parent
     {
